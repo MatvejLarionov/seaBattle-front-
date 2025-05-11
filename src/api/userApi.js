79d12eb690299ed27aaf22ade297abbe8650e15b
Matvey <matvejlarionovit@gamil.com>
2025-05-11 14:37:44 +0300

    create registration

--- src/api/userApi.js
+++ src/api/userApi.js
@@ -2,8 +2,12 @@ import { Axios } from "axios";
 import { serverUrl } from "./serverUrl";
 
 export const userApi = {
-  userAxios: new Axios({ baseURL: `${serverUrl}/users`, headers: { "Content-Type": "application/json" } }),
+  userAxios: new Axios({
+    baseURL: `${serverUrl}/users`,
+    headers: { "Content-Type": "application/json" }
+  }),
   postUser(user) {
     return this.userAxios.post("/registration", JSON.stringify(user))
+      .then(res => JSON.parse(res.data))
   }
 }
--- src/components/Registration/Registration.jsx
+++ src/components/Registration/Registration.jsx
@@ -1,19 +1,36 @@
+import { useState } from "react"
 import { userApi } from "../../api/userApi"
 import styles from "./Registration.module.css"
 export default function Registration() {
-
+  const [error, setError] = useState(" ")
+  const errorMessages = {
+    loginRepeat: "such login already exists",
+    emptyFields: "fill in the fields",
+    passwordIsNotCorrect: "The password must contain a symbol other than numbers and its size must be greater than eight"
+  }
   return (
     <div className={styles.container}>
       <form className={styles.form}>
         <input className={styles.login} type="text" placeholder="login" id="inpLogin" />
         <input className={styles.password} type="password" placeholder="password" id="inpPassword" />
+        <p className={styles.error}>{error}</p>
         <button onClick={event => {
           event.preventDefault()
           const user = {
             login: document.getElementById("inpLogin").value.trim(),
             password: document.getElementById("inpPassword").value.trim()
           }
-          userApi.postUser(user).then(res=>console.log(res.data))
+          if (!user.login || !user.password) {
+            setError(errorMessages.emptyFields)
+            return
+          }
+          userApi.postUser(user).then(res => {
+            if (res.error)
+              setError(errorMessages[res.error])
+            else {
+              console.log(res)
+            }
+          })
         }} className={styles.submit}>submit</button>
       </form>
     </div>
