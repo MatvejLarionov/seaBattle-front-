27454612e3a6c26b06bf145036cc20daf973fbc0
Matvey <matvejlarionovit@gamil.com>
2025-05-11 18:22:09 +0300

    create mainPage

--- src/components/MainPage/MainPage.jsx
+++ src/components/MainPage/MainPage.jsx
@@ -1,9 +1,18 @@
+import styles from "./MainPage.module.css"
 import { useContext } from "react"
 import { UserDataContext } from "../../context/UserDataContext"
 
 export default function MainPage() {
   const { user, setUser } = useContext(UserDataContext)
   return (
-    <div>{user.login}</div>
+    <div className={styles.container}>
+      <header className={styles.header}>
+        <img className={styles.avatar} src={user.avatar || "http://localhost:3000/img/ava.jpg"} alt="" />
+        <p>{user.login}</p>
+      </header>
+      <main className={styles.main}>
+        <button className={styles.btnPlay}>play</button>
+      </main>
+    </div>
   )
 }
