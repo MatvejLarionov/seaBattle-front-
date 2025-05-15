114ccb26666f7331630e758bb2831cabe66a1841
Matvey <matvejlarionovit@gamil.com>
2025-05-15 16:48:16 +0300

    create MiniUserMenu

--- src/components/MainPage/MainPage.tsx
+++ src/components/MainPage/MainPage.tsx
@@ -1,16 +1,10 @@
 import styles from "./MainPage.module.css"
-import { useContext } from "react"
-import { UserDataContext } from "../../context/UserDataContext"
-import { serverUrl } from "../../api/serverUrl"
+import MiniUserMenu from "../MiniUserMenu/MiniUserMenu"
 
 export default function MainPage() {
-  const { user } = useContext(UserDataContext)
   return (
     <div className={styles.container}>
-      <header className={styles.header}>
-        <img className={styles.avatar} src={user.avatar || `${serverUrl}/usersAvatars/defaultAvatar.jpg`} alt="" />
-        <p>{user.login}</p>
-      </header>
+      <MiniUserMenu />
       <main className={styles.main}>
         <button className={styles.btnPlay}>play</button>
       </main>
